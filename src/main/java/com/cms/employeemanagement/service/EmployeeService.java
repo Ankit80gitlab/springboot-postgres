f12437f93e52131829a68aece298185365fce565
Ankit Prajapati <ankitprajapati.user@gmail.com>
2024-02-16 16:26:01 +0530

    commit 1

--- src/main/java/com/cms/employeemanagement/service/EmployeeService.java
+++ src/main/java/com/cms/employeemanagement/service/EmployeeService.java
@@ -28,16 +28,16 @@ public class EmployeeService {
     }
 
     public Employee login(Employee employee) {
-        System.out.println(employee.getEmail()+" "+employee.getPassword());
         Employee emp = new Employee();
-        emp.setEmail(employee.getEmail());
-        emp.setPassword(employee.getPassword());
+//        emp.setEmail(employee.getEmail());
+//        emp.setPassword(employee.getPassword());
         boolean verified = false;
 
         List<Employee> list = employeeRepository.findAll();
         for(Employee e : list){
-            if(e.getEmail().equals(emp.getEmail()) && e.getPassword().equals(emp.getPassword())){
+            if(e.getEmail().equals(employee.getEmail()) && e.getPassword().equals(employee.getPassword())){
                 verified = true;
+                emp = e;
                 break;
             }
         }
